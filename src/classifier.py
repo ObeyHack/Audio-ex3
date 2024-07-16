28f75da10261030141a0c7398ab1882c32f5e4ce
sean <Sean.Cherny.Israel13@gmail.com>
2024-07-16 17:11:04 +0300

    data as torch.utils.data.DataLoader for easy usage

--- src/classifier.py
+++ src/classifier.py
@@ -4,3 +4,16 @@ import torch.nn as nn
 import numpy as np
 import load_data.loader as loader
 
+
+
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
+        self.lstm = nn.LSTM(20, 20, 2, bidirectional=True)
+        self.linear = nn.Linear(40, 9)
+
+    def forward(self, x):
+        x, _ = self.lstm(x)
+        x = self.linear(x)
+        return x
+
--- src/load_data/loader.py
+++ src/load_data/loader.py
@@ -1,12 +1,31 @@
 import os
 import librosa
 import numpy as np
+import torch
 from librosa.feature import mfcc
+from torch.utils import data
 
 zero_to_eight = {0: 'zero', 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven', 8: 'eight'}
+BATCH_SIZE = 16     # Batch size
+T = 16000           # Input sequence length
+
+class data_set(data.Dataset):
+    def __init__(self,X,Y):
+        self.X = X                           # set data
+        self.Y = Y                           # set lables
+
+    def __len__(self):
+        return len(self.X)                   # return length
+
+    def __getitem__(self, idx):
+        return [self.X[idx], self.Y[idx]]
+
 
 def extract_mfcc(file_path):
     y, sr = librosa.load(file_path, sr=None)
+    # pad the signal to have the same length if it is shorter than T
+    if len(y) < T:
+        y = np.pad(y, (0, T - len(y)), 'constant')
     return mfcc(y=y, sr=sr)
 
 
@@ -37,15 +56,27 @@ def get_set(set, root_path="../data"):
     for i in range(len(zero_to_eight)):
         for file in paths[i]:
             X.append(extract_mfcc(file))
+    X = np.array(X)
 
-    return X, Y
+    return torch.tensor(X), torch.tensor(Y)
 
 
 def load_data():
     train_X, train_Y = get_set('train')
-    validation_X, validation_Y = get_set('validation')
+    validation_X, validation_Y = get_set('val')
     test_X, test_Y = get_set('test')
-    return {'train': (train_X, train_Y), 'validation': (validation_X, validation_Y), 'test': (test_X, test_Y)}
+
+    # Define the datasets
+    train_dataset = torch.utils.data.TensorDataset(train_X, train_Y)
+    validation_dataset = torch.utils.data.TensorDataset(validation_X, validation_Y)
+    test_dataset = torch.utils.data.TensorDataset(test_X, test_Y)
+
+    # Define the dataloaders
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+    validation_loader = torch.utils.data.DataLoader(validation_dataset, batch_size=BATCH_SIZE, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=True)
+
+    return {'train': train_loader, 'validation': validation_loader, 'test': test_loader}
 
 
 if __name__ == '__main__':
